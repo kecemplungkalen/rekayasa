8ae21dfc193540f0ef722f41ef767c4da71d7b64
kecemplungkalen <revolusigalang@gmail.com>
2013-04-17 16:09:25 +0700

    edit modal css

--- application/views/header_view.php
+++ application/views/header_view.php
@@ -5,12 +5,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
     <link rel="stylesheet" href="<?php echo base_url(); ?>include/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>include/css/bootstrap-modal.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>include/css/bootstrap-responsive.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>include/css/custom.css">
+
     <script src="<?php echo base_url(); ?>include/js/jquery.min.js"></script>
     <script src="<?php echo base_url(); ?>include/js/bootstrap.min.js"></script>
   </head>
   
 <body>
-	
-	<!--navbar -->
