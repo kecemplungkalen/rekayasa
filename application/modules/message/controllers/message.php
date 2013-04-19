7534feb5c6e3d69dee9b6576a0b8d776945ef105
kecemplungkalen <revolusigalang@gmail.com>
2013-04-19 09:21:56 +0700

    tembah

--- application/modules/message/controllers/message.php
+++ application/modules/message/controllers/message.php
@@ -7,6 +7,7 @@ Class Message extends MX_Controller{
 		parent::__construct();
 		
 		$this->load->model('message_model');
+		$this->load->model('label_model');
 	}
 	
 	public function index($label)
@@ -17,20 +18,56 @@ Class Message extends MX_Controller{
 			$data =$this->message_model->getlabel($label);
 			if($data)
 			{
-				$isi = new stdClass;
 				foreach($data as $dt)
 				{
-					$inbox[] = $dt->id_inbox;
+					$isi['id_inbox'] = $dt->id_inbox;
+					$isi['total'] = $dt->total;
+					$isi['id_user'] = $dt->id_user;
+					$isi['recive_date'] = $dt->recive_date;
+					$isi['content'] = $dt->content;
+					$isi['read_status'] = $dt->read_status;
+					$isi['id_address_book'] = $dt->id_address_book;
+					$isi['first_name'] = $dt->first_name;
+					$isi['last_name'] = $dt->last_name;
+					$isi['number'] = $dt->number;
+					$isi['email'] = $dt->email;
+					//$isi['create_date'] = $dt->create_date;
+					//$isi['last_update'] = $dt->last_update;
+					$data_label = $this->label_model->get_by_id_inbox($dt->id_inbox);
+					if($data_label)
+					{
+						foreach($data_label as $dl)
+						{
+							$sub['id_label'] = $dl->id_label;
+							$sub['name'] = $dl->name;
+							$sub['color'] = $dl->color;
+							$sub['additional'] = $dl->additional;
+							$sub_data[] = $sub; 
+						}
+					}
+					$isi['label'] = $sub_data;
+					//$isi['id_label'] = $id_label;
+					//$isi['id_labelname'] = $dt->id_labelname;
+					//$isi['name'] = $name;
+					//$isi['color'] = $color;
+					//$isi['additional'] = $additional;
+					$remap[] = $isi;
+					
 				}
-				$isi->inbox = $inbox;
-				var_dump($isi);
 				
-			}		
+				$view['data'] = $remap;
+				
+			}
+			
+			
+			
+			
+					
 			$this->load->view('header_view');
 			$this->load->view('navbar_view');
 			$this->load->view('sidebar_view');
 			$this->load->view('inbox/top_button_view');
-			$this->load->view('dashboard/dashboard_view');
+			$this->load->view('dashboard/dashboard_view',$view);
 			//$this->load->view('modal/address_modal_edit');
 			//$this->load->view('modal/address_modal_group');
 			$this->load->view('footer_view');
--- application/views/dashboard/dashboard_view.php
+++ application/views/dashboard/dashboard_view.php
@@ -14,31 +14,37 @@
 	</thead>
     <tbody>
 	<?php if(isset($data)){ ?>
-		<?php //var_dump($data);?>
-		<?php foreach($data as $dt){ ?>
-
+		<?php for($i=0;$i< count($data);$i++) { ?>
 		<tr>
 			<td>
 				<input type="checkbox"> 
 			</td>
 			<td>
 				<a href="#">
-					<?php if($dt->read_status == '0'){?>
-					<strong><?php echo $dt->first_name;?> <?php echo $dt->last_name ;?><br><?php echo $dt->number;?> <?php if($dt->total != '1'){?> (<?php echo $dt->total;  ?>)<?php } ?> </strong>
+					<?php if($data[$i]['read_status'] == '0'){?>
+					<strong><?php echo $data[$i]['first_name'];?> <?php echo $data[$i]['last_name'] ;?><br><?php echo $data[$i]['number'];?> <?php if($data[$i]['total'] != '1'){?> (<?php echo $data[$i]['total'];  ?>)<?php } ?> </strong>
 					<?php } else {?>
-					<?php echo $dt->first_name;?> <?php echo $dt->last_name ;?><br><?php echo $dt->number;?>  <?php if($dt->total != '1'){?> (<?php echo $dt->total;  ?>)<?php } ?> 
+					<?php echo $data[$i]['first_name'];?> <?php echo $data[$i]['last_name'] ;?><br><?php echo $data[$i]['number'];?>  <?php if($data[$i]['total'] != '1'){?> (<?php echo $data[$i]['total'];  ?>)<?php } ?> 
 
 					<?php }?>
 				</a>
 			</td>
 			<td>
-				2 April 2013<br>
-				<small>02:00PM</small>
+				<?php echo date('j F Y',$data[$i]['recive_date']);?><br>
+				<small><?php echo date('g:i a',$data[$i]['recive_date']);?><br></small>
 			<td>
-				1234567890 1234567890 1234567890 1234567890 1234567890 1234567890...
+				<?php echo $data[$i]['content'] ;?>
 			</td>
         	<td>
-				<a href="tag"><span class="label badge-b6cff5"><small>Konfirmasi</small></span></a><a href="removetag"><span class="label badge-b6cff5"><small>x</small></span></a>
+				<?php $label = $data[$i]['label']; ?>
+				<?php //var_dump(count($label));?>
+				<?php for($j=0;$j< count($label);$j++){ ?>
+					<?php if($label[$j]['additional'] != '0') {?>
+					
+					<a href="message/<?php echo $label[$j]['name']; ?>"><span class="label badge-<?php echo $label[$j]['color']; ?>"><small><?php echo $label[$j]['name']; ?></small></span></a><a href="removetag"><span class="label badge-<?php echo $label[$j]['color']; ?>"><small>x</small></span></a>
+					<?php } ?>
+				<?php } ?>
+				
 			</td>
 		</tr>
 
