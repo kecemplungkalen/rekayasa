d1a06c6c189501128e21f189e84a97a574278805
kecemplungkalen <revolusigalang@gmail.com>
2013-05-23 13:49:28 +0700

    edit view limit

--- application/modules/config/views/config_sendlimit_view.php
+++ application/modules/config/views/config_sendlimit_view.php
@@ -6,9 +6,11 @@
 				<td><strong>Phone ID</strong></td>
 				<td><strong>Time Limit</strong></td>
 				<td><strong>Sending Limit</strong></td>
+				<!--
 				<td><strong>Total Send</strong></td>
 				<td><strong>Total UnSend</strong></td>
 				<td><strong>Status</strong></td>
+				-->
 			</tr>
 		</thead>
 		<tbody>	
@@ -20,16 +22,18 @@
 				<td><a href="#" onclick="edit_config_sendlimit('<?php echo $sl->id_config_modem; ?>')"  ><?php echo $sl->phoneID; ?></a></td>
 				<td><?php echo $sl->time_sending_limit; ?></td>
 				<td><?php echo $sl->sending_limit; ?></td>
-				<td><?php echo $sl->total_send; ?></td>
-				<td><?php echo $sl->total_unsend; ?></td>
-				<td><?php if($sl->status_sending == 'ready'){ ?>
-				<span class="label label-success"><?php echo $sl->status_sending; ?></span>
-				<?php } elseif($sl->status_sending == 'pending') {?>
-				<span class="label label-warning"><?php echo $sl->status_sending; ?></span>
-				<?php } else { ?>
-				<span class="label label-success"><?php echo $sl->status_sending; ?></span>
-				<?php } ?>
+			<!--
+				<td><?php //echo $sl->total_send; ?></td>
+				<td><?php //echo $sl->total_unsend; ?></td>
+				<td><?php //if($sl->status_sending == 'ready'){ ?>
+				<span class="label label-success"><?php // echo $sl->status_sending; ?></span>
+				<?php //} elseif($sl->status_sending == 'pending') {?>
+				<span class="label label-warning"><?php // echo $sl->status_sending; ?></span>
+				<?php// } else { ?>
+				<span class="label label-success"><?php // echo $sl->status_sending; ?></span>
+				<?php //} ?>
 				</td>
+			-->
 			</tr>
 			<?php }?>
 		<?php }?>
