3a797d7b80becf7812db95128f2738a9412a6dc8
kecemplungkalen <revolusigalang@gmail.com>
2013-05-27 15:08:02 +0700

    cek pulsa command

---
+++ application/modules/config/controllers/atcommand.php
@@ -0,0 +1,27 @@
+<?php 
+
+Class Atcommand extends MX_Controller{
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('ussd_helper');
+	}
+	
+	function getcommand()
+	{
+		$data = $_POST;
+		var_dump($data);
+		if(is_array($data))
+		{
+			$pulsa = ussd_number($data['param'],$data['port']);
+			if($pulsa)
+			{
+				return $pulsa;
+				
+			}
+		}
+		return FALSE;
+	}
+	
+}
